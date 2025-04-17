f568f0d8c939ebd9f8f35448f8a06487419659a2
ttd2089 <ttd2089@gmail.com>
2025-04-16 23:13:55 -0300

    chore: log deprecation notice

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const github = require('@actions/github');
 
 try {
 
+  core.warning(`⚠️ This action is no longer maintained.
+    Use an inline run step instead.
+    Example: https://github.com/ttd2089/secret-file-action/blob/main/.github/workflows/alternative.yml.`);
+
   const secret = core.getInput('secret');
 
   const decoded = core.getBooleanInput('b64-decode')
